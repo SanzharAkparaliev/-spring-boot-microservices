5bcfeb63acbbbd0fd644db1186ae90877f3c8570
Sanzhar <taalaibekuulusanzhar@gmail.com>
2022-10-02 15:30:04 +0600

    service discovery

--- inventory-service/src/main/java/com/programming/invertoryservice/InvertoryServiceApplication.java
+++ inventory-service/src/main/java/com/programming/invertoryservice/InvertoryServiceApplication.java
@@ -5,9 +5,11 @@ import com.programming.invertoryservice.repository.InventoryRepository;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class InvertoryServiceApplication {
 
     public static void main(String[] args) {
